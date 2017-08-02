1d117d7b7cd0ec63debae1a71433d06aff98ed15
ecti <erictsai1010@gmail.com>
2017-08-02 00:03:49 -0700

    js - longest increasing subsequence 2

--- javascript/dynamic-programming/longest-increasing-subsequence.js
+++ javascript/dynamic-programming/longest-increasing-subsequence.js
@@ -4,32 +4,31 @@
  *  For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}
  *  or [10, 9, 2, 5, 3, 7, 101, 18] is 4 and LIS is [2, 3, 7, 101]
  */
-
 const longestIncreasingSubsequence = (seq) => {
-	const storage = [];
+	const longest = [];
+
 	for (let i = 0; i < seq.length; i++) {
-		storage[i] = 1;
+		longest.push(1);
 	}
 
-	let maxSeq = 1;
 	for (let i = 1; i < seq.length; i++) {
 		let count = 0;
 		for (let j = 0; j < i; j++) {
-			if (seq[i] > seq[j]) {
-				count = Math.max(storage[j], count);
+			if (seq[j] < seq[i]) {
+				count = Math.max(count, longest[j]);
 			}
 		}
-
-		storage[i] = 1 + count;
-		maxSeq = Math.max(storage[i], maxSeq);
+		longest[i] = 1 + count;
 	}
 
-	return maxSeq;
+	return longest[seq.length - 1];
 };
 
 // Driver program
 (() => {
 	const seq1 = [10, 9, 2, 5, 3, 7, 101, 18];
+	const seq2 = [10, 22, 9, 33, 21, 50, 41, 60, 80];
 
 	console.log(longestIncreasingSubsequence(seq1));
+	console.log(longestIncreasingSubsequence(seq2));
 })();
