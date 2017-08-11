3355f02c321f38f27e5e27dba1024edf01e63fa2
ecti <erictsai1010@gmail.com>
2017-08-10 22:34:34 -0700

    js - preorder traversal with stack

--- javascript/binary-tree/preorder-traversal.js
+++ javascript/binary-tree/preorder-traversal.js
@@ -19,8 +19,35 @@ const preorderTraversal = (root, result) => {
     return result;
 };
 
+const preorderTraversalStack = (root) => {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const stack = [];
+    stack.push(root);
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        result.push(node.data);
+
+        if (node.right) {
+            stack.push(node.right);
+        }
+        if (node.left) {
+            stack.push(node.left);
+        }
+    }
+
+    return result;
+};
+
 // Driver program
 (() => {
     const array = [1, 2, 3, 4, 5];
+    
     console.log(preorderTraversal(binaryTree.serialize(array)));
+
+    console.log(preorderTraversalStack(binaryTree.serialize(array)));
 })();
